0f9446fff53a8eb6e5ff06b39c07fb8fe23c97dc
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-02-28 15:53:33 -0300

    feat: create view edit profile user auth

---
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Pages\Auth\EditProfile as BaseEditProfile;
+
+class EditProfile extends BaseEditProfile
+{
+    protected static ?string $navigationIcon = 'heroicon-o-shield-check';
+// Path profile edit personalizado
+    protected static string $view = 'filament.pages.auth.edit-profile';
+    protected static string $layout = 'filament-panels::components.layout.index';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Grid::make(3)
+                    ->schema([
+
+//                        Section::make()
+//                            ->schema([
+//                                FileUpload::make('avatar')
+//                                    ->label('')
+//                                    ->disk('public')
+//                                    ->directory('setting_images'),
+//                            ])
+//                            ->columnSpan(1),
+
+                        Section::make()
+                            ->schema([
+                                $this->getNameFormComponent(),
+                                $this->getEmailFormComponent(),
+                                $this->getPasswordFormComponent(),
+                                $this->getPasswordConfirmationFormComponent(),
+                            ])
+                            ->columnSpan(3),
+                    ]),
+            ])->statePath('data')->columns([
+                'default' => 2,
+                'sm' => 1,
+                'md' => 2,
+                'lg' => 2,
+                'xl' => 2,
+                '2xl' => 2
+            ]);
+    }
+
+    protected function hasFullWidthFormActions(): bool
+    {
+        return false;
+    }
+
+    public static function getSlug(): string
+    {
+        return static::$slug ?? 'perfil';
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\EditProfile;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -29,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile(EditProfile::class)
             ->brandName(config('app.name'))
             ->favicon(asset('images/brands/icon-340.png'))
             ->colors([
